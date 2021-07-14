aefb39174da0cc6149776f4aee1cc09eefd5e3b7
Oliver Koch <github@oltoko.de>
2021-07-14 22:20:09 +0200

    Publish version 0.2.0 of PathCopy

--- pathcopy.rb
+++ pathcopy.rb
@@ -1,15 +1,15 @@
 class Pathcopy < Formula
     desc "A cli command which copies the absolute Path of the given file or directory to the clipboard."
     homepage "https://github.com/oltoko/PathCopy"
-    version "0.1.1"
+    version "0.2.0"
     if OS.mac?
         url "https://github.com/oltoko/PathCopy/releases/download/#{version}/pc_#{version}_macos.tar.gz"
         # curl -L https://github.com/oltoko/PathCopy/releases/download/x.x.x/pc_x.x.x_macos.tar.gz | shasum -a 256
-        sha256 "e12b735e6b795d24b832d3b6b6d99d2507e838e376385784b033b4197916ca9f"
+        sha256 "8b56e109021e44095353b648fe5800dc3692589ff5204dc9e3234d83b6e1ab4b"
     elsif OS.linux?
         url "https://github.com/oltoko/PathCopy/releases/download/#{version}/pc_#{version}_linux.tar.gz"
         # curl -L https://github.com/oltoko/PathCopy/releases/download/x.x.x/pc_x.x.x_linux.tar.gz | shasum -a 256
-        sha256 "c43e651a05af127c429b30a97dabe8e196ec401f54a2fd39a3bdfdf0d0ca9d84"
+        sha256 "75035fe0bd045055da7f20c3b970bd0edadfd4d7f2c81716f4398aa20c0527e2"
     end
 
     def install
